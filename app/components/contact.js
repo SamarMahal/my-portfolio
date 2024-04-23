06acb2681c58cd59234fedb33682b71639e7db5b
SamarMahal <141462664+SamarMahal@users.noreply.github.com>
2024-04-22 19:01:38 -0600

    Update contact.js

--- app/components/contact.js
+++ app/components/contact.js
@@ -10,27 +10,7 @@ const Contact = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
             <div>
                 <h2 className="text-3xl md:text-4xl font-bold text-gray-800 mb-6">Get in Touch</h2>
-                <p className="text-gray-600 mb-6">Have a project in mind or just want to say hi? Drop me a message using the form below.</p>
-                <div className="flex items-center space-x-4 mb-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M4 4a1 1 0 011-1h10a1 1 0 011 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1V4zm0 6a1 1 0 011-1h10a1 1 0 011 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-3z" clipRule="evenodd" />
-                    </svg>
-                    <p className="text-gray-600">Southern Alberta Institute Of Technology</p>
-                </div>
-                <div className="flex items-center space-x-4 mb-6">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M10 2a8 8 0 00-8 8c0 3.301 2.891 7.051 7.04 12.185a.725.725 0 001.12 0C17.109 17.051 20 13.301 20 10a8 8 0 00-8-8zm0 2a6 6 0 00-6 6c0 2.598 2.082 5.548 6 9.566 3.918-4.018 6-6.968 6-9.566a6 6 0 00-6-6z" clipRule="evenodd" />
-                        <path fillRule="evenodd" d="M10 11a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
-                    </svg>
-                    <p className="text-gray-600">samardeepsingh@edu.sait.ca</p>
-                </div>
-                <div className="flex items-center space-x-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M10 2a8 8 0 00-8 8c0 3.301 2.891 7.051 7.04 12.185a.725.725 0 001.12 0C17.109 17.051 20 13.301 20 10a8 8 0 00-8-8zm0 2a6 6 0 00-6 6c0 2.598 2.082 5.548 6 9.566 3.918-4.018 6-6.968 6-9.566a6 6 0 00-6-6z" clipRule="evenodd" />
-                        <path fillRule="evenodd" d="M10 11a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
-                    </svg>
-                    <p className="text-gray-600">(403) 707 8851</p>
-                </div>
+                <p className="text-gray-600 mb-6">Have a project in mind or just want to say hi? Drop me a message using the form!</p>
             </div>
             <div>
                 <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
